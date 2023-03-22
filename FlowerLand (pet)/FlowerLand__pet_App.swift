f505b0c3ddea98f1cbbfd03a22b4bf50316d52cc
Arthur <olehno.arturs@gmail.com>
2023-03-23 00:23:59 +0200

    Initial Commit

---
+++ FlowerLand (pet)/FlowerLand__pet_App.swift
@@ -0,0 +1,17 @@
+//
+//  FlowerLand__pet_App.swift
+//  FlowerLand (pet)
+//
+//  Created by Артур Олехно on 23/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlowerLand__pet_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
